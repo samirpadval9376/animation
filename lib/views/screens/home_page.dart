4889f8ebf2d2632de50bf34249c72070e623f1f0
Samir Padval <samirpadval9376@gmail.com>
2023-08-06 14:17:49 +0530

    commit

--- lib/views/screens/home_page.dart
+++ lib/views/screens/home_page.dart
@@ -1,6 +1,4 @@
 import 'dart:math';
-
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,82 +13,272 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     Size s = MediaQuery.of(context).size;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Animation",
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-          ),
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+        fit: BoxFit.fill,
+        image: AssetImage(
+          "assets/images/stars.jpg",
         ),
-        centerTitle: true,
-        elevation: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
+      )),
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(20),
           child: Column(
             children: [
-              Container(
-                height: s.height * 0.8,
-                width: s.width,
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  borderRadius: BorderRadius.circular(15),
+              Expanded(
+                flex: 10,
+                child: TweenAnimationBuilder(
+                  duration: const Duration(
+                    seconds: 10,
+                  ),
+                  curve: Curves.easeInOut,
+                  tween: Tween(
+                    begin: 0.0,
+                    end: 2 * pi,
+                  ),
+                  builder: (context, val, _) {
+                    return Transform.translate(
+                      // offset: Offset(30, -90),
+                      offset: Offset(-130, -90),
+                      child: Transform.rotate(
+                        angle: val,
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Transform.scale(
+                              scale: 1.3,
+                              child: Container(
+                                height: double.infinity,
+                                width: double.infinity,
+                                decoration: const BoxDecoration(
+                                  color: Colors.transparent,
+                                  shape: BoxShape.circle,
+                                ),
+                                alignment: Alignment.center,
+                                child: Transform.scale(
+                                  scale: 0.8,
+                                  child: Transform.translate(
+                                    offset: Offset(-0, -5),
+                                    child: Image.network(
+                                      "https://www.numerologistpro.com/images/planets/sun1000.png",
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Transform.translate(
+                              offset: const Offset(0, -200),
+                              child: Container(
+                                height: 150,
+                                width: 150,
+                                margin: const EdgeInsets.all(10),
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                      "assets/images/jupiter.png",
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Transform.translate(
+                              offset: const Offset(120, -160),
+                              child: Container(
+                                height: 150,
+                                width: 150,
+                                margin: const EdgeInsets.all(10),
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                      "assets/images/jupiter.png",
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Transform.translate(
+                              offset: const Offset(190, -40),
+                              child: Container(
+                                height: 150,
+                                width: 150,
+                                margin: const EdgeInsets.all(10),
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                      "assets/images/jupiter.png",
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Transform.translate(
+                              offset: const Offset(185, 90),
+                              child: Container(
+                                height: 150,
+                                width: 150,
+                                margin: const EdgeInsets.all(10),
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                      "assets/images/jupiter.png",
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Transform.translate(
+                              offset: const Offset(90, 180),
+                              child: Container(
+                                height: 150,
+                                width: 150,
+                                margin: const EdgeInsets.all(10),
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                      "assets/images/jupiter.png",
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Transform.translate(
+                              offset: const Offset(-40, 190),
+                              child: Container(
+                                height: 150,
+                                width: 150,
+                                margin: const EdgeInsets.all(10),
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                      "assets/images/jupiter.png",
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Transform.translate(
+                              offset: const Offset(-155, 125),
+                              child: Container(
+                                height: 150,
+                                width: 150,
+                                margin: const EdgeInsets.all(10),
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                      "assets/images/jupiter.png",
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Transform.translate(
+                              offset: const Offset(-190, 0),
+                              child: Container(
+                                height: 150,
+                                width: 150,
+                                margin: const EdgeInsets.all(10),
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                      "assets/images/jupiter.png",
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Transform.translate(
+                              offset: const Offset(-140, -140),
+                              child: Container(
+                                height: 150,
+                                width: 150,
+                                margin: const EdgeInsets.all(10),
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                      "assets/images/jupiter.png",
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
                 ),
-                child: SizedBox(
-                  width: s.width,
+              ),
+              Expanded(
+                child: AnimatedDefaultTextStyle(
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  duration: const Duration(
+                    seconds: 20,
+                  ),
                   child: TweenAnimationBuilder(
                     duration: const Duration(
-                      seconds: 5,
+                      seconds: 10,
+                    ),
+                    tween: Tween(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
                     ),
                     builder: (context, val, _) {
                       return Align(
                         alignment: val,
-                        child: TweenAnimationBuilder(
-                          duration: const Duration(
-                            seconds: 5,
-                          ),
-                          tween: Tween(
-                            begin: 0.0,
-                            end: 2 * pi,
-                          ),
-                          curve: Curves.bounceOut,
-                          builder: (context, val, _) {
-                            return Transform.rotate(
-                              angle: val,
-                              child: CarouselSlider(
-                                items: [
-                                  Image.asset(
-                                    "assets/images/jupiter.png",
-                                    height: 150,
-                                  ),
-                                  Image.asset(
-                                    "assets/images/jupiter.png",
-                                    height: 150,
-                                  ),
-                                ],
-                                options: CarouselOptions(
-                                  autoPlay: true,
-                                ),
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                width: s.width,
+                                child: const Text("data1"),
                               ),
-                            );
-                          },
+                              Container(
+                                width: s.width,
+                                child: const Text("data2"),
+                              ),
+                              Container(
+                                width: s.width,
+                                child: const Text("data3"),
+                              ),
+                            ],
+                          ),
                         ),
                       );
                     },
-                    tween: Tween(
-                      begin: Alignment.centerRight,
-                      end: Alignment.centerLeft,
-                    ),
                   ),
                 ),
               ),
             ],
           ),
         ),
+        backgroundColor: Colors.transparent,
       ),
     );
   }
